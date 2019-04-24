734cecd58ea9acd6a57da5bc7d0c2e89528ae4bd
Tom <thomas.bozon@campu_numerique.fr>
2019-04-24 16:19:07 +0200

    4-destructuring ok

--- 4-destructuring/script.js
+++ 4-destructuring/script.js
@@ -3,11 +3,17 @@
 const [a, b] = [1, 2, 3, 4]
 console.log(a)
 console.log(b)
+// les elements du tableau sont appelés par rapport a la position des variables 
 
-const {first, last} = {first: 'Paul', last: 'Henta', age: 35}
+const {first, last, age} = {first: 'Paul', last: 'Henta', age: 35}
 console.log(first)
 console.log(last)
+console.log(age)
+//les elements de l'objet sont appelés par rapport au nom de la clé
+
 
 //4.2 Application
-
+Object.values(data).forEach(value => {
+    console.log(value.dateDebut,value.texte,value.dateFin)
+})
 console.log(data)
